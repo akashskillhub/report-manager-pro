abc61c7cafda886b330b9c351df27a20e42b0950
akash <akash@gmail.com>
2023-06-07 10:31:46 +0530

    07-06-2023

--- client/src/admin/pages/AddDoctor.jsx
+++ client/src/admin/pages/AddDoctor.jsx
@@ -1,71 +1,142 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const AddDoctor = () => {
 
+  const [address, setAddress] = useState([
+    { type: "main clinic", val: "fake street" }
+  ])
 
-    const table = <table class="table table-dark table-striped table-hover">
-      <thead>
-        <tr>
-          <th scope="col">#</th>
-          <th scope="col">First</th>
-          <th scope="col">Last</th>
-          <th scope="col">Handle</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <th scope="row">1</th>
-          <td>Mark</td>
-          <td>Otto</td>
-          <td>@mdo</td>
-        </tr>
-        <tr>
-          <th scope="row">2</th>
-          <td>Jacob</td>
-          <td>Thornton</td>
-          <td>@fat</td>
-        </tr>
-        <tr>
-          <th scope="row">3</th>
-          <td colspan="2">Larry the Bird</td>
-          <td>@twitter</td>
-        </tr>
-      </tbody>
-    </table>
-  
+  const table = <table class="table table-dark table-striped table-hover">
+    <thead>
+      <tr>
+        <th scope="col">#</th>
+        <th scope="col">First</th>
+        <th scope="col">Last</th>
+        <th scope="col">Handle</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <th scope="row">1</th>
+        <td>Mark</td>
+        <td>Otto</td>
+        <td>@mdo</td>
+      </tr>
+      <tr>
+        <th scope="row">2</th>
+        <td>Jacob</td>
+        <td>Thornton</td>
+        <td>@fat</td>
+      </tr>
+      <tr>
+        <th scope="row">3</th>
+        <td colspan="2">Larry the Bird</td>
+        <td>@twitter</td>
+      </tr>
+    </tbody>
+  </table>
+
+  /*
+    name                            M
+    mobile multiple with lable      M
+    hospital / clinic name          M
+    address / multile lable         M                                        
+    gender                          M
+    avatar                          O
+    education                       O
+    dob                             O
+    date of anv.                    O
+    email                           O
+  */
+
   return <>
-  <div className="container">
-    <button className='btn btn-primary' data-bs-toggle="modal" data-bs-target="#doctorModal"> Add Doctor</button>
-    <div className="row">
-{table}
+    <div className="container">
+      <div className='text-end my-3'>
+
+        <button className='btn btn-primary' data-bs-toggle="modal" data-bs-target="#doctorModal"> Add Doctor</button>
+      </div>
+      <div className="row">
+        {table}
+      </div>
     </div>
-  </div>
- 
-  {/* modal doctor start */}
- 
-  
-  
-  <div class="modal fade" id="doctorModal" >
-    <div class="modal-dialog">
-      <div class="modal-content">
-        <div class="modal-header">
-          <h5 class="modal-title" id="doctorModal">Add Doctor's details</h5>
-          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-        </div>
-        <div class="modal-body">
-            <input className='form-control' type="text" name="" id="doctorName"  placeholder='Enter doctors name'/><br /> 
-            <input className='form-control' type="text" name="" id="doctorEmail" placeholder='Enter doctors email' /><br /> 
-            <input className='form-control' type="text" name="" id="doctorMobile" placeholder='Enter doctors Mobile' /><br /> 
-            
-        </div>
-        <div class="modal-footer">
-          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-          <button type="button" class="btn btn-primary" data-bs-dismiss="modal">Save changes</button>
+
+    {/* modal doctor start */}
+
+
+
+    <div class="modal fade" id="doctorModal" >
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="doctorModal">Add Doctor's details</h5>
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+          </div>
+          <div class="modal-body">
+
+            <pre>
+              {JSON.stringify(address, null, 2)}
+            </pre>
+
+            {
+              address.map((item, i) => <div className='input-group my-3'>
+                <select class="form-select" onChange={e => {
+                  const copy = [...address]
+                  copy[i].type = e.target.value
+                  setAddress(copy)
+                }}>
+                  <option >Choose Type</option>
+                  <option value="Primary">Primary</option>
+                  <option value="Clinic">Clinic</option>
+                  <option value="Home">Home</option>
+                </select>
+                <input
+                  className='form-control '
+                  type="text"
+                  name=""
+                  value={address[i].val}
+                  id="doctorMobile"
+                  onChange={e => {
+                    const copy = [...address]
+                    copy[i].val = e.target.value
+                    setAddress(copy)
+                  }
+                  }
+                  placeholder='Enter doctors Address' />
+                {
+                  i === 0
+                    ? <button
+                      type="button"
+                      disabled={address.length >= 2}
+                      class="btn btn-primary"
+                      onClick={e => setAddress([
+                        ...address,
+                        { type: "new address", val: "fake street" }
+                      ])}
+                    >+</button>
+                    : <button
+                      type="button"
+                      class="btn btn-danger"
+                      onClick={e => {
+                        const copy = [...address]
+                        copy.splice(i, 1)
+                        setAddress(copy)
+                      }}
+                    >-</button>
+                }
+
+              </div>)
+            }
+
+
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+            <button type="button" class="btn btn-primary" data-bs-dismiss="modal">Save changes</button>
+          </div>
         </div>
       </div>
     </div>
-  </div>
-  {/* modal doctor end */}
+    {/* modal doctor end */}
   </>
 }
 
--- client/src/share/pages/Login.jsx
+++ client/src/share/pages/Login.jsx
@@ -1,9 +1,100 @@
+import { useFormik } from 'formik'
 import React from 'react'
+import * as yup from 'yup'
+import { Link } from "react-router-dom"
 
 const Login = () => {
-    return (
-        <div>Login</div>
-    )
+    const formik = useFormik({
+        initialValues: ({
+            email: "",
+            password: "",
+            account: "doctor"
+        }),
+        validationSchema: yup.object({
+            email: yup.string().required().email(),
+            password: yup.string().required(),
+            account: yup.string().required(),
+        }),
+        onSubmit: (value, restForm) => {
+            // restForm(),
+            console.log(value);
+        }
+    })
+
+    return <>
+        <div className="container my-3">
+            <div className="row">
+                <div className="col-sm-6 offset-sm-3">
+                    <div className="card">
+                        <div className="card-header fw-bold fs-3 text-bg-danger text-center">Login</div>
+                        <form onSubmit={formik.handleSubmit}>
+                            <div className="card-body">
+                                <div>
+                                    <label htmlFor="email" className="form-label">Email</label>
+                                    <input
+                                        type="text"
+                                        {...formik.getFieldProps("email")}
+                                        className={`form-control ${formik.touched.email && (formik.errors.email ? "is-invalid" : "is-valid")}`}
+                                        id="email"
+                                        placeholder="Enter Your Email"
+                                    />
+                                    <div className="valid-feedback">Looks good!</div>
+                                    <div className="invalid-feedback">Please Enter a Valid Email.</div>
+                                </div>
+                                <div className="mt-2">
+                                    <label htmlFor="password" className="form-label">Password</label>
+                                    <input
+                                        type="password"
+                                        {...formik.getFieldProps("password")}
+                                        className={`form-control ${formik.touched.password && (formik.errors.password ? "is-invalid" : "is-valid")}`}
+                                        id="password"
+                                        placeholder="Enter Your Password"
+                                    />
+                                    <div className="valid-feedback">Looks good!</div>
+                                    <div className="invalid-feedback">Please choose a username.</div>
+                                </div>
+                                <div className='mt-3'>
+                                    <p className='d-inline me-4 fw-bold'>Login As</p>
+                                    <div className="form-check-inline">
+                                        <input
+                                            {...formik.getFieldProps("account")}
+                                            className="form-check-input"
+                                            type="radio"
+                                            name="account"
+                                            checked={formik.values.account === "doctor"}
+                                            value="doctor"
+                                            id="doctor" />
+                                        <label className="form-check-label mx-2" htmlFor="doctor">
+                                            Doctor
+                                        </label>
+                                    </div>
+                                    <div className="form-check-inline">
+                                        <input
+                                            {...formik.getFieldProps("account")}
+                                            className="form-check-input"
+                                            type="radio"
+                                            name="account"
+                                            checked={formik.values.account !== "doctor"}
+                                            value="pathology"
+                                            id="pathology" />
+                                        <label className="form-check-label mx-2" htmlFor="pathology">
+                                            Pathology
+                                        </label>
+                                    </div>
+                                </div>
+                                <button type="submit" className="btn btn-danger w-100 mt-3">
+                                    Login
+                                </button>
+                                <p className="text-center mt-3">
+                                    Dont Have Account? <Link to="/register" className='text-danger'>Create Account</Link>
+                                </p>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </>
 }
 
 export default Login
